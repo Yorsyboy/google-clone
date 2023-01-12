7ade9a4393fd0839ff443879d0e2f065e7755693
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-01-13 00:32:38 +0100

    Create body section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import { MicrophoneIcon, SearchIcon } from '@heroicons/react/solid'
 
 export default function Home() {
   return (
@@ -15,12 +16,22 @@ export default function Home() {
       {/* Header */}
       <Header />
       {/* Body */}
-      <form>
+      <form className='flex flex-col items-center mt-40 '>
         <Image
           width={300}
           height={100}
           src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
         />
+        <div 
+        className='flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl'>
+          <SearchIcon className='h-5 text-gray-500 mr-3'/>
+          <input type="text" className='flex-grow focus:outline-none' />
+          <MicrophoneIcon className='h-5'/>
+        </div>
+        <div className='flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center'>
+        <button className='btn'>Google Search</button>
+        <button className='btn'>I'm feeling lucky</button>
+        </div>
       </form>
 
       {/* Footer */}
