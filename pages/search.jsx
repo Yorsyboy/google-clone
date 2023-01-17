8f5b74a2fc51fa236a11916dfc4626dfb7d14f8c
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-01-18 00:35:34 +0100

    Add image result search

--- pages/search.jsx
+++ pages/search.jsx
@@ -3,10 +3,11 @@ import SearchHeader from "../components/SearchHeader";
 import SearchResults from "../components/SearchResults";
 import Response from "../Response";
 import { useRouter } from "next/router";
+import ImageResult from "../components/ImageResult";
 
-export default function search({results}) {
+export default function search({ results }) {
   const router = useRouter();
-  console.log(results)
+  console.log(results);
   return (
     <div>
       <Head>
@@ -16,20 +17,28 @@ export default function search({results}) {
       {/*Search Header */}
       <SearchHeader />
 
-      {/* Search Results */}
-      <SearchResults results={results}/>
+      {/* Search web and images Results */}
+      {router.query.searchType === "image" ? (
+        <ImageResult results={results} />
+      ) : (
+        <SearchResults results={results} />
+      )}
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
   const startIndex = context.query.start || "1";
-  const mockData = false; 
-  const data = mockData ? Response : await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${
-      process.env.CX_KEY
-    }&q=${context.query.term}${context.query.searchType && "&searchType=image"}&start=${startIndex}`
-  ).then((response) => response.json());
+  const mockData = false;
+  const data = mockData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          process.env.API_KEY
+        }&cx=${process.env.CX_KEY}&q=${context.query.term}${
+          context.query.searchType && "&searchType=image"
+        }&start=${startIndex}`
+      ).then((response) => response.json());
   return {
     props: {
       results: data,
