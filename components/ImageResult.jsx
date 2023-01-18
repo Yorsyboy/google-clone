282b6b5f3585aaf2b43562a09e41210a837529f4
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-01-18 10:05:44 +0100

    fix component name

--- components/ImageResult.jsx
+++ components/ImageResult.jsx
@@ -1,6 +1,6 @@
 import PaginationButtons from "./PaginationButtons";
 
-export default function ({ results }) {
+export default function ImageResult ({ results }) {
   return (
     <div className="mt-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
